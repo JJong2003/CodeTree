1f9256c9e1dd2fbb3c08d1cb26a94d30004c5560
JongHyeon Park <67833043+JJong2003@users.noreply.github.com>
2025-12-31 11:15:07 +0900

    Title: 집합의 원소, Time: 281ms, Memory: 32MB, Status: Wrong Answer - Codetree

---
+++ 251231/집합의 원소/elements-of-a-set.py
@@ -0,0 +1,32 @@
+n, m = map(int, input().split())
+query = [list(map(int, input().split())) for _ in range(m)]
+
+# Please write your code here.
+array = [i for i in range(n + 1)]
+
+
+def _find(a):
+    if array[a] != a:
+        array[a] = _find(array[a])
+    return array[a]
+
+
+def _union(a, b):
+    pa = _find(a)
+    pb = _find(b)
+
+    if pa == pb:
+        return
+
+    if pa < pb:
+        array[b] = array[a]
+    else:
+        array[a] = array[b]
+
+
+for i in range(m):
+    cmd, a, b = query[i]
+    if cmd == 0:
+        _union(a, b)
+    else:
+        print(1 if _find(a) == _find(b) else 0)
