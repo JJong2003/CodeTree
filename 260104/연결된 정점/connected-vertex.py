20881d885be7d9c66ee1d93165a3e4ae4747693f
JongHyeon Park <67833043+JJong2003@users.noreply.github.com>
2026-01-04 19:03:42 +0900

    Title: 연결된 정점, Time: 1092ms, Memory: 37MB, Status: Time Limit Exceed - Codetree

---
+++ 260104/연결된 정점/connected-vertex.py
@@ -0,0 +1,33 @@
+n, m = map(int, input().split())
+
+uf = [i for i in range(n+1)]
+size = [1 for i in range(n+1)]
+
+def _find(a):
+    if uf[a] == a:
+        return a
+    uf[a] = _find(uf[a])
+    return uf[a]
+
+def _union(a, b):
+    pa = _find(a)
+    pb = _find(b)
+
+    if size[pa] < size[pb]:
+        uf[pa] = pb
+        size[pb] += size[pa]
+    else:
+        uf[pb] = pa
+        size[pa] += size[pb]
+
+
+for _ in range(m):
+    op, *nums = input().split()
+    if op == "x":
+        a, b = map(int, nums)
+        _union(a, b)
+    else:
+        a = int(nums[0])
+        print(size[_find(a)])
+
+
